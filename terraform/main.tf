3595fdb622aaea636954c5ff97a8d554c192a806
HamzaF3366 <hamza.farooq@expertflow.com>
2025-09-22 01:59:34 +0500

    Updated the terraform file to create VMs

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,35 +12,31 @@ provider "google" {
   project = var.project_id
   region  = var.region
   zone    = var.zone
-  # Authentication will use GOOGLE_APPLICATION_CREDENTIALS env var set by Jenkins
+  # Authentication uses GOOGLE_APPLICATION_CREDENTIALS env var set by Jenkins
 }
 
-resource "google_compute_network" "vpc_network" {
-  name                    = "k8s-network"
-  auto_create_subnetworks = true
-}
-
-resource "google_compute_firewall" "k8s_fw" {
-  name    = "k8s-fw"
-  network = google_compute_network.vpc_network.name
+# Firewall to allow SSH + Kubernetes traffic on the default network
+resource "google_compute_firewall" "allow_ssh_k8s" {
+  name    = "allow-ssh-k8s"
+  network = "default"
 
   allow {
     protocol = "tcp"
     ports    = [
       "22",            # SSH
-      "80",            # HTTP
-      "443",           # HTTPS
+      "80", "443",     # HTTP/HTTPS
       "6443",          # Kubernetes API server
       "2379-2380",     # etcd
-      "10250", "10251", "10252", # kubelet & control plane
+      "10250", "10251", "10252", # kubelet + control plane
       "30000-32767"    # NodePort services
     ]
   }
 
-  source_ranges = ["10.132.0.0/20"]
-  target_tags   = ["k8s"]
+  # For testing open to all (restrict to Jenkins IP in production)
+  source_ranges = ["0.0.0.0/0"]
 }
 
+# Create 2 Ubuntu VMs
 resource "google_compute_instance" "k8s_vm" {
   count        = var.vm_count
   name         = "k8s-vm-${count.index}"
@@ -55,14 +51,14 @@ resource "google_compute_instance" "k8s_vm" {
   }
 
   network_interface {
-    network = google_compute_network.vpc_network.name
-    access_config {}
+    network = "default"
+    access_config {} # required for external/public IP
   }
 
   tags = ["k8s"]
 
   metadata = {
-    # remove any newline characters from the public key so metadata entry is valid
+    # Add Jenkins SSH public key to "ubuntu" user
     ssh-keys = "${var.ssh_user}:${replace(var.jenkins_ssh_pub, "\n", "")}"
   }
 }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,14 @@
-output "master_internal_ip" {
-  description = "Internal IP of master (first VM)"
-  value       = google_compute_instance.k8s_vm[0].network_interface[0].network_ip
+# Public IP of master (first VM)
+output "master_public_ip" {
+  description = "Public IP of the master VM"
+  value       = google_compute_instance.k8s_vm[0].network_interface[0].access_config[0].nat_ip
 }
 
-output "worker_internal_ips" {
-  description = "List of internal IPs of workers"
-  value       = [for idx, inst in google_compute_instance.k8s_vm : inst.network_interface[0].network_ip if idx != 0]
+# Public IPs of worker nodes (all except master)
+output "worker_public_ips" {
+  description = "List of public IPs of worker VMs"
+  value       = [
+    for idx, inst in google_compute_instance.k8s_vm :
+    inst.network_interface[0].access_config[0].nat_ip if idx != 0
+  ]
 }
